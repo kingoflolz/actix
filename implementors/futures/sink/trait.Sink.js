6223109b49d1273bb7ba22753c1bb7d2c1a1f42d
Travis CI User <travis@example.org>
2017-11-09 23:39:22 +0000

    Documentation upload

---
+++ implementors/futures/sink/trait.Sink.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix"] = ["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/sink/trait.Sink.html\" title=\"trait futures::sink::Sink\">Sink</a> for &amp;'a UnboundedSender&lt;T&gt;","impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/sink/trait.Sink.html\" title=\"trait futures::sink::Sink\">Sink</a> for &amp;'a UnboundedSender&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
